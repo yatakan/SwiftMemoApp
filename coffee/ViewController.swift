0a56a5ba612169c19deeb90dce698d6a2d20cabb
yatakan <takaya@fuzaketanamaetsukeruna.local>
2017-08-19 16:58:53 +0900

    Initial Commit

---
+++ coffee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  coffee
+//
+//  Created by taka on 2017/08/19.
+//  Copyright © 2017年 takayan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
